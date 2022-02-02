b376b4f1dc12df6c202c2bcebf03a4468162eb72
Linecutterx <40548573+Linecutterx@users.noreply.github.com>
2022-02-02 20:16:48 +0000

    Update custom_domain.sh
    Cosmetic changes!
    Fixed a typo in the comments & the missing "." in the .bak file creation.

--- utils/custom_domain.sh
+++ utils/custom_domain.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# This script is written by a NewsBlur user, lineuctterx, and is unmaintained. Use at your own risk.
+# This script is written by a NewsBlur user, linecutterx, and is unmaintained. Use at your own risk.
 #
 
 if [ $# -eq 0 ]
@@ -19,10 +19,10 @@ if [ $# -eq 0 ]
       if [ $# -eq 2 ]
       then
        echo "$(tput setaf 1)Changinging newsblur_web/docker_local_settings.py: $(tput setaf 2)$1 to $2$(tput sgr 0)"
-       sed -i'bak' -e "/NEWSBLUR_URL/ s/$1/$2/" -e "/SESSION_COOKIE_DOMAIN/ s/$1/$2/" './newsblur_web/docker_local_settings.py' 
+       sed -i'.bak' -e "/NEWSBLUR_URL/ s/$1/$2/" -e "/SESSION_COOKIE_DOMAIN/ s/$1/$2/" './newsblur_web/docker_local_settings.py' 
        head -20 './newsblur_web/docker_local_settings.py'
        echo "$(tput setaf 1)Changinging config/fixtures/bootstrap.json: $(tput setaf 2)$1 to $2$(tput sgr 0)"
-       sed -i'bak' -e "/domain/ s/$1/$2/" './config/fixtures/bootstrap.json'
+       sed -i'.bak' -e "/domain/ s/$1/$2/" './config/fixtures/bootstrap.json'
        head -10 './config/fixtures/bootstrap.json'
       fi
     fi
